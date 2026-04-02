36f822f53696d5181c663b25a320bf502d541fc3
Jack Schultz <contact@jackschultz.dev>
2026-04-02 22:12:50 +1030

    Feature/solve permission errors (#15)
    * Updated join message and Added command info to /help
    
    * Fixed fail to send lack of permissions message if the channel has no category
    
    * Fixed creating broken hourglass vcs when bot doesn't have administrator by removing overwrites from edit. It is no longer needed as overwrites should never be empty {}.
    
    * Fixed log messages and displaying category when permissions lack
    
    * Added explicit permission check instead of asking for permissions on error when creating voice channel
    
    * Updated README.md and requirements.txt to make sure discord.py is not installed and pycord isn't broken

--- cogs/general/embeds.py
+++ cogs/general/embeds.py
@@ -7,21 +7,61 @@ class DonateEmbed(discord.Embed):
         self.color = discord.Color.green()
         self.title = "💚 A Message from the Developer"
         self.description = (
-            "Thank you for using Robotnic! 🤖\n"
-            "This is a **[FOSS](<https://wikipedia.org/wiki/Free_and_open-source_software>)** project with a **free** public instance developed and hosted by "
-            "[MeltedButter77](https://github.com/MeltedButter77)."
+            "Thank you for using Robotnic!\n"
+            "This is a **[FOSS](<https://wikipedia.org/wiki/Free_and_open-source_software>)** project with a **free** public instance developed and hosted by [MeltedButter77](https://github.com/MeltedButter77)."
         )
         self.add_field(
             name="💸 Donations",
             value=(
-                "Robotnic took a long time to develop and also **costs money to host**, "
-                "which MeltedButter currently pays out of his own pocket."
+                "Robotnic took a long time to develop and also **costs money to host**, which MeltedButter currently pays out of his own pocket."
             ),
             inline=False
         )
         self.add_field(
             name="🙏 Please Consider Supporting the Developer",
             value="Every bit of support helps keep Robotnic running smoothly. ❤️",
+            inline=False
+        )
+        self.set_footer(text="📩 Need more help? Reach out to support below!")
+
+
+class HelpEmbed(discord.Embed):
+    def __init__(self):
+        super().__init__()
+        self.color = discord.Color.green()
+        self.title = "Command List"
+        self.description = (
+        )
+        self.add_field(
+            name="/setup",
+            value=(
+                "Use this menu to create new `Creator Channel`s by clicking the green \"Make new Creator\" button or edit existing `Creator Channel`s using the dropdown list."
+            ),
+            inline=False
+        )
+        self.add_field(
+            name="/settings controls",
+            value="Allows changing the controls available to channel owners. Every button is togglable and you can choose between labeled buttons, icons or a dropdown menu as controls. You can also adjust if the owner gets pinged on channel creation.",
+            inline=False
+        )
+        self.add_field(
+            name="/settings logging",
+            value="Allows for setting a log channel, if set, selected events will be logged in that channel. To customise the list, simply deselect the ones you would not like to include.",
+            inline=False
+        )
+        self.add_field(
+            name="/settings profanity_filter",
+            value="While still basic, this setting allows for disabling or only sending an alert if the profanity filter is triggered rather than blocking the action.",
+            inline=False
+        )
+        self.add_field(
+            name="/donate /support",
+            value="Returns with information on how to support Robotnic's uptime.",
+            inline=False
+        )
+        self.add_field(
+            name="/ping",
+            value="Returns the bot's latency",
             inline=False
         )
         self.set_footer(text="📩 Need more help? Reach out to support below!")
