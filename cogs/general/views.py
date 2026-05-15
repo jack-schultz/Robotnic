c8ed1b48f26129726fee74e20c9726a09a75bcf2
MeltedButter77 <contact@jackschultz.dev>
2026-05-15 13:06:56 +0930

    Updated outdated links

--- cogs/general/views.py
+++ cogs/general/views.py
@@ -10,7 +10,7 @@ class ButtonsView(discord.ui.View):
         self.add_item(
             discord.ui.Button(
                 label="Support the Developer",
-                url="https://github.com/sponsors/MeltedButter77",
+                url="https://github.com/sponsors/jack-schultz",
                 emoji="💖",
                 style=discord.ButtonStyle.link
             )
